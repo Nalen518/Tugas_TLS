6dfcf51471a9e363cf581004934a5890b87fc11b
Nalen518 <nalen.r.sadewa@gmail.com>
2024-09-15 01:43:30 +0700

    Tugas.TLS.cpp

--- Tugas.TLS.cpp
+++ Tugas.TLS.cpp
@@ -8,11 +8,18 @@ int main (){
     cout << "Enter the radius of the circle :";
     cin >> radius;
 
+    if (radius > 0){
     area = M_PI * pow (radius,2);
     circumference =2*M_PI*radius;
 
     cout << "Area of the circle : " << area << endl;
     cout << "circumference of the circle :" << circumference << endl;
+
+     } else{
+        cout << "ERROR : The number that you input must bigger than zero" << endl;
+     }
+  
+
   
     return 0;
 
